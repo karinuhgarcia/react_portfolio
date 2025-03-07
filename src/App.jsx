d2fa8310c4cb29e75e12fb616f160c07830f92bb
Karina Garcia <karinag.000@gmail.com>
2025-03-06 17:17:02 -0700

    added layout, header/footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div id='mainPage'>
+      <Header />
+      <div>HHEHEHEHEHE</div>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div>Header</div>
+            <nav>
+                <a>About Me</a>
+                <a>Portfolio</a>
+                <a>Contact</a>
+                <a>Resume</a>
+            </nav>
+        </header>
+    )
+}
