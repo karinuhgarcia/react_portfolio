48a2433d63250e7428091c5c0f42602a97fccdcf
Karina Garcia <karinag.000@gmail.com>
2025-03-08 15:58:40 -0700

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Aboutme from './components/Aboutme'
 
 function App() {
 
   return (
     <div id='mainPage'>
       <Header />
+      <Aboutme />
       <div>HHEHEHEHEHE</div>
       <Footer />
     </div>
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AboutMe.css'
+
+export default function Aboutme() {
+    return (
+        <section id="aboutme">
+            <h2>About Me</h2>
+            <img src="/imgs/karina_avatar.png" alt="Avatar Image of Karina Garcia" />
+            <p>Hi! I'm Karina Garcia, a software engineer based in New York City.
+                I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
+                My goal is to always build products that provide pixel-perfect, performant</p>
+        </section>
+    )
+}
